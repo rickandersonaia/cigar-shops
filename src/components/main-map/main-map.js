a0c6dd3b72ac69c6c2d65a3273ad75a0e8b22379
Rick Anderson <rick@byobwebsite.com>
2018-05-29 07:19:58 -0700

    Added fail condition for reading JSON main-map-model
    feat:  If main-map fails to read the json data file it will fall back
    to the default of Palm Springs

--- src/components/main-map/main-map.js
+++ src/components/main-map/main-map.js
@@ -5,37 +5,55 @@ define(['knockout', 'text!./main-map.html'], function (ko, templateMarkup) {
         var self = this;
 
         self.cityList = ko.observableArray([]);
-        self.message = ko.observable();
-
-        var mapData = $.getJSON("components/main-map/main-map-model.json", function (data) {
-            self.cityList(data);
-            console.log('success');
+        self.currentCity = ko.observableArray([]);
+        self.message = ko.computed(function () {
+            return self.currentCity().name + " Cigar Shops";
         });
 
-        mapData.done( function (data) {
-            var acity = ChooseCity(data);
+        var cityData = $.getJSON("components/main-map/main-map-model.json", function (data) {
+            self.cityList(data);
+            console.log('Successfully read city data from JSON file');
+        });
 
-            self.message(data[acity].name);
+        cityData.done( function (data) {
+            var curCity = chooseCurrentCity(data);
+            self.currentCity(data[curCity]);
+            city = new google.maps.LatLng(data[curCity].lat, data[curCity].lng);
+            currentCityMap(city);
+        })
 
-            city = new google.maps.LatLng(data[acity].lat, data[acity].lng);
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: city,
-                zoom: 10,
-                zoomControlOptions: {
-                    position: google.maps.ControlPosition.LEFT_CENTER,
-                    style: google.maps.ZoomControlStyle.SMALL
-                },
-                streetViewControlOptions: {
-                    position: google.maps.ControlPosition.LEFT_BOTTOM
-                },
-                mapTypeControl: false,
-                panControl: false
-            });
+        cityData.fail(function () {
+            console.log('Failed to city data from JSON file')
+            var defaultCity = {
+                    "name": "Palm Springs",
+                    "lat": "33.7667",
+                    "lng": "-116.3592",
+                    "zoom": "10"
+                };
+            self.currentCity(defaultCity);
+            city = new google.maps.LatLng(defaultCity.lat, defaultCity.lng);
+            currentCityMap(city);
         })
     }
 
-    function ChooseCity(data){
+    function chooseCurrentCity(data){
         return 'la';
+    }
+
+    function currentCityMap(city){
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: city,
+            zoom: 10,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.LEFT_CENTER,
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            streetViewControlOptions: {
+                position: google.maps.ControlPosition.LEFT_BOTTOM
+            },
+            mapTypeControl: false,
+            panControl: false
+        });
     }
 
 
